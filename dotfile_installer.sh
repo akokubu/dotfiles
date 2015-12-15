89bab5ff008aea80811fefff04d8235f560e64ba
Atsushi Kokubu <akokubu@longposition.co.jp>
2015-12-15 15:50:22 +0900

    zshrcの整理

--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshrc_config ~/.zshrc_config
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -snf ~/dotfiles/.zshrc_config ~/.zshrc_config
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
+if [[ ! -f ~/.zsh/antigen/antigen.zsh ]]; then
+    cd ~/.zsh
+    git clone https://github.com/zsh-users/antigen.git
+fi
