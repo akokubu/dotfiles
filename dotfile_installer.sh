964e39a22e9971106d00c47f48eb32ff4be124c0
Atsushi Kokubu <akokubu@longposition.co.jp>
2015-12-15 13:07:53 +0900

    gitconfigを追加

--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshrc_config ~/.zshrc_config
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+
